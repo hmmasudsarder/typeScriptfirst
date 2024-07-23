51362f25e29d9cb3b7b880cbd0a3d02871d1b643
hmmasudsarder <hmmasudsarder1@gmail.com>
2024-07-23 17:15:49 +0600

    added destracting

--- src/1.8.ts
+++ src/1.8.ts
@@ -1,12 +1,23 @@
 {
-const poorUser = {
-    name: "Masud",
-    balance: 0,
-    addBalance(balance: number): string{
-        return `My new balance is: ${this.balance+ balance}`;
-    }
+// const poorUser = {
+//     name: "Masud",
+//     balance: 0,
+//     addBalance(balance: number): string{
+//         return `My new balance is: ${this.balance+ balance}`;
+//     }
+// }
+// const data = {
+//     date: "change"
+// }
+
+const user = {
+    id: 3422,
+    name: {
+        firstName: "Masud Sarder",
+        middleName: "sarder",
+        lastName: "hridoy",
+    },
+    phone: '02033030330',
 }
-const data = {
-    date: "change"
-}
+const {phone, name:{middleName}} = user;
 }
